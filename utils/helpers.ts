f44a3f1d30e2bbae92a7bb9509eb4ad5ff41f0a0
Joaquín Galasso <joacogalasso@gmail.com>
2025-08-02 01:37:39 -0300

    .

--- utils/helpers.ts
+++
@@ -1,7 +0,0 @@
-export const formatTime = (seconds: number): string => {
-  if (isNaN(seconds)) return '00:00';
-  const floorSeconds = Math.floor(seconds);
-  const min = Math.floor(floorSeconds / 60);
-  const sec = floorSeconds % 60;
-  return `${min.toString().padStart(2, '0')}:${sec.toString().padStart(2, '0')}`;
-};
